f5e7fe8c3ee9179bd8acf5127cf6c642037230b0
Willie Chalmers III <willieechalmers@gmail.com>
2019-09-21 14:57:04 -0500

    Fix submissions route

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express.Router()
 var codeRoutes = require('./coderoutes')
-var userDataRoutes = require('./userDataRoutes')
+var userDataRoutes = require('./submissions')
 
 app.use('/code', codeRoutes)
 app.use('/submissions', userDataRoutes)
