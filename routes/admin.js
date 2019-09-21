c48e6be48bb7f25af28733d976e3971b92dae17e
Willie Chalmers III <willieechalmers@gmail.com>
2019-09-21 05:46:28 -0500

    Create route to create new bill submission

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,9 @@
 var express = require('express')
 var app = express.Router()
 var codeRoutes = require('./coderoutes')
+var userDataRoutes = require('./userDataRoutes')
 
 app.use('/code', codeRoutes)
+app.use('/submissions', userDataRoutes)
 
 module.exports = app
---
+++ routes/userDataRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express.Router();
+const UserSubDoc = require('../model/UserSubsDoc')
+
+app.post('/upload', (req, res) => {
+    if (!req.body) {
+        res.status(401).json({ error: 'no-params' })
+        return
+    }
+    // TODO: Validate document
+    if (!(req.body['drgCode'] && req.body['hospitalId'] && req.body['cost'])) {
+        res.status(400).json({ error: 'not-all-params-specified' })
+    }
+    const uploadData = {
+        drgCode: req.body.drgCode,
+        hospitalId: req.body.hospitalId,
+        cost: req.body.cost,
+    }
+    const userDoc = new UserSubDoc(uploadData)
+    try {
+        await userDoc.save()
+        req.status(200).json({ success: true })
+    } catch (e) {
+        res.status(401).json({ error: 'db-write-error' })
+    }
+})
