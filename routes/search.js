08c5b824ea2cf23991f98673a12e7c30829a2ce3
Willie Chalmers III <willieechalmers@gmail.com>
2019-09-21 12:30:01 -0500

    Add search functionality
    TODO: Perform lookup by description

--- routes/search.js
+++ routes/search.js
@@ -1,14 +1,41 @@
 const express = require('express')
+const {
+    isDrg,
+    lookupDrgInformation,
+    findDrgFromQuery
+} = require('../domain/searchUtils')
+
 const route = express()
 
 /**
  * Params: 
  *  - query (String): A search query or DRG code
- *  - loc (String): A city to lookup
- *  - hospitals (String|Array<String>): A list of hospital codes
+ * 
+ * Returns:
+ *  - A list of matching DRGCodeInfo, 0 if none exist
  */
-route.get('/search', async (req, res) => {
-
+route.get('/', async (req, res) => {
+    const searchQuery = req.query.query || req.body.query
+    if (typeof searchQuery === 'undefined') {
+        res.status(200).json({ error: 'query-undefined' })
+    }
+    if (isDrg(searchQuery)) {
+        console.log(searchQuery)
+        try {
+            const result = await lookupDrgInformation(searchQuery)
+            res.status(400).json([ result ])
+        } catch (e) {
+            res.status(500).json({ error: 'internal' })
+        }
+    } else {
+        try {
+            console.log(searchQuery)
+            const results = await findDrgFromQuery(searchQuery)
+            res.status(400).json(results)
+        } catch (e) {
+            res.status(500).json({ error: 'internal' })
+        }
+    }
 })
 
 
